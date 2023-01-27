733ae27741641a07fb883baf3478aac85d398cfa
AyaMu <ayalamuruch@gmail.com>
2023-01-27 14:40:32 -0500

    create StarshipDetails fetch request

--- src/pages/StarshipDetails/StarshipDetails.jsx
+++ src/pages/StarshipDetails/StarshipDetails.jsx
@@ -4,6 +4,19 @@ import { Link, useLocation } from "react-router-dom";
 
 
 const StarshipDetails = () => {
+  const [starshipDetails, setStarshipDetails] = useState({})
+
+  const location = useLocation()
+
+  useEffect(()=> {
+    const fetchStarshipDetails = async () => {
+      const StarshipData = await getStarshipDetails
+      (location.state.starship.url)
+      setStarshipDetails(StarshipData)
+    }
+    fetchStarshipDetails()
+  }, [location.state.starship.url])
+
   return ( 
     <div>
       Starship Details Component!
